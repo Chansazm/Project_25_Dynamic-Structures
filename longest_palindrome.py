8697522ce100008d63eaf003e386b48141118828
Chansa Chishimba <chansazm@yahoo.co.uk>
2021-03-22 07:22:38 -0500

    Added longest palindrome

---
+++ longest_palindrome.py
@@ -0,0 +1,25 @@
+class solution:
+
+    def longest_palindrome(self,s):
+        res = ""
+        for i in range(len(s)):
+            odd = self.helperpalindrome(s,i,i)
+            even = self.helperpalindrome(s,i,i+1)
+            res = max(odd,even,res,key=len)
+        return res
+        
+    
+    
+    
+    
+    def helperpalindrome(self,s,l,r):
+        while l >= 0 and r <= len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
+                 
+        
+        
+s = "babad"
+r = len(s)-1
+print(solution.helperpalindrome(s,0,r)
