ff12b43aca19a5209d24a4f9e8b86caedcd3841c
Chansa Chishimba <chansazm@yahoo.co.uk>
2021-09-30 08:53:36 -0700

    Added gas station

--- longest_substring.py
+++ longest_substring.py
@@ -1,19 +1,21 @@
-def longest_substring(s):
-    n = len(s)
-    map = {}
+def longest_substring(string):
+    dict = {}
+    window_start = 0 
     window_end = 0
     result = 0  
-    window_start = 0  
+     
     
     
-    for window_end in range(n):
-        if s[window_end] in map:
-            window_start = max(window_start,map[s[window_end]])
-        result = max(result,window_end - window_start+1)
-        map[s[window_end]] = window_end + 1
+    for window_end in range(len(string)):
+        if string[window_end] in dict:
+            window_start = max(window_start,dict[string[window_end]])
+        result = max(result,window_end - window_start)
+        dict[string[window_end]] = window_end
     return result
 
 #driver
 v = "abcabcbb"
+ #      ^
+ #   ^
 print(longest_substring(v))
         
